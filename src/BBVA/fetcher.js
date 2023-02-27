034f6b9b041fd396fb3432a8bcd51fa9376d0ca3
TomasCImach <tomas90@gmail.com>
2023-02-27 09:49:01 -0300

    bbva fetcher

---
+++ src/BBVA/fetcher.js
@@ -0,0 +1,100 @@
+require("dotenv").config();
+const axios = require('axios');
+const {
+  MongoClient,
+  ServerApiVersion
+} = require("mongodb");
+const fs = require("fs");
+const isLocal = false;
+var mongoUrl = '';
+if (isLocal) {
+  mongoUrl = "mongodb://localhost:27017/?maxPoolSize=20&w=majority";
+} else {
+  mongoUrl = process.env.MONGO_URL;
+}
+
+const mongo_database = 'Benefits'
+const mongo_collection = 'BBVA_GO'
+
+const MAX_RETRIES = 50;
+const RETRY_DELAY = 1000; // 1 second
+async function makeRequestWithBackoff(url, retries = 0) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.status === 403 && retries < MAX_RETRIES) {
+      console.log(`Rate limited. Retrying in ${RETRY_DELAY}ms...`);
+      await new Promise(resolve => setTimeout(resolve, RETRY_DELAY));
+      return makeRequestWithBackoff(url, retries + 1);
+    }
+    throw error;
+  }
+}
+
+async function run() {
+  console.log("app start")
+  try {
+    const client = new MongoClient(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    await client.connect();
+    console.log("client connected")
+
+    let benefitsIds = [];
+
+    let continueFetching = true
+    let pager = 0
+    let destacado = 's';
+    while (continueFetching) {
+      await makeRequestWithBackoff(`https://go.bbva.com.ar/fgo/API/v1/communications?pager=${pager}&destacado=${destacado}`)
+        .then((res) => {
+          console.log(`pager ${pager}, destacado ${destacado}`)
+          // let resData = res.data
+          let resData = res
+          if (resData.data.length < 1) {
+            if (destacado === 's') {
+              destacado = 'n'
+              pager = 0
+              console.log("changing to destacado = n")
+              return
+            } else {
+              continueFetching = false
+              return
+            }
+          }
+          for (let item of resData.data) {
+            // console.log("item", item)
+            benefitsIds.push(item.id)
+          }
+          pager++;
+        })
+        .catch(error => {
+          console.log(`Error fetching data for pager=${pager}&destacado=${destacado}: ${error.message}`);
+        })
+    }
+    // console.log("benefitsIds", benefitsIds)
+
+    let okArray = []
+    for (let id of benefitsIds) {
+      await makeRequestWithBackoff(`https://go.bbva.com.ar/fgo/API/v1/communication/${id}`)
+        .then((res) => {
+          console.log(`Getting benefit ${id}`)
+          let data = res;
+          okArray.push(data.data)
+        })
+        .catch(error => {
+          console.log(`Error fetching data for ${id}: ${error.message}`);
+        })
+    }
+
+    // console.log("okArray", okArray)
+    await client.db(mongo_database).collection(mongo_collection).insertMany(okArray)
+    fs.writeFile('temp/okFile.json', JSON.stringify(okArray), function (err) {
+      if (err) throw err;
+      console.log('Array saved to file!');
+      process.exit();
+    });
+  } catch (error) {
+    console.log("error BBVA", error);
+  }
+}
+run().catch(console.dir);
